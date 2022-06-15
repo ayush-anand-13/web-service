e965be2fd430a35f8af5df1c96a2631cb4ade989
FNU Ayush Anand <ayushaf@amazon.com>
2022-06-15 11:38:50 -0700

    apple

--- src/app.py
+++ src/app.py
@@ -86,7 +86,7 @@ def appleConnect(filename):
     #    "username": username,
     #}
     #return render_template("index.html", **context)
-    return send_from_directory("./well-known", filename)
+    return send_from_directory("./.well-known/", filename)
 
 
 ################
